cafd300d2242590de7f0cf579c9a18632e9b61fe
Alekya32 <n.alekhyasingh@gmail.com>
2017-05-20 15:35:35 +0530

    array using custom directives

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 var app=angular.module("myApp",[])
 app.controller("myController",["$scope",function($scope){
 	$scope.message="Good Morning:)";
+	$scope.companyName="TechMinds Pvt Ltd";
+	$scope.userDetails=[
+		{name: "Robin Sharma",address:"california,United States"},
+		{name:"savi Sharma",address:"Bhopal,India"},
+		{name:"Tolstoy" ,address:"USA"}]
+	$scope.changeCompanyName=function(){
+		$scope.companyName="Techmind Solutions";
+	}
+
 }])
 
 app.directive("myCustomerDetailsDirective",function(){
 	return{
 		restrict:'EACM',
-		template:"<h3>Customer details</h3>",
-		replace:true
+		/*template:"<h3>Custom directive:  {{message}}</h3>",*/
+		templateUrl:'customerDetails.html',
+		replace:true,
+		scope:{
+				userName:'@',
+				userDetails:'=',
+				changeName:'&'
+		}
 	}
 })
