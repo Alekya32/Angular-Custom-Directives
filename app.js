626db6b0d93a8985edc5214371b29ab235730ffa
Alekya32 <n.alekhyasingh@gmail.com>
2017-05-20 16:43:36 +0530

    Compile and link

--- app.js
+++ app.js
@@ -22,6 +22,38 @@ app.directive("myCustomerDetailsDirective",function(){
 				userName:'@',
 				userDetails:'=',
 				changeName:'&'
-		}
+		},
+		/*compile:function(element,attributes){
+			console.log("In compiler.................")
+			console.log(element.html());
+			console.log(attributes);
+
+			return{
+				pre:function(scope,elems,attrs){
+					console.log("Pre link........");
+					console.log(scope.userDetails);
+					console.log(elems);
+					console.log(attrs);
+					if(scope.userDetails.name=="Tolstoy"){
+						elems.removeAttr('class')
+					}
+				},
+				post:function(scope,elems,attrs){
+					console.log("Post link !!!!!");
+					console.log(scope);
+					console.log(elems);
+					console.log(attrs);
+				}
+				
+			}
+		}*/
+
+		link:function(scope,elems,attrs){
+					console.log("Post link !!!!!");
+					console.log(scope);
+					console.log(elems);
+					console.log(attrs);
+				}
+		
 	}
 })
